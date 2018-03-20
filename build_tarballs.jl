77ad1709b0fcc6b01332b43315034a5b35c296c8
staticfloat <staticfloat@gmail.com>
2018-03-19 23:46:55 -0700

    Add autogenerated files to build libffi

---
+++ build_tarballs.jl
@@ -0,0 +1,45 @@
+using BinaryBuilder
+
+# Collection of sources required to build libffi
+sources = [
+    "https://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz" =>
+    "d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd libffi-3.2.1/
+./configure --prefix=$prefix --host=$target
+make -j${nproc}
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    BinaryProvider.Linux(:i686, :glibc),
+    BinaryProvider.Linux(:x86_64, :glibc),
+    BinaryProvider.Linux(:aarch64, :glibc),
+    BinaryProvider.Linux(:armv7l, :glibc),
+    BinaryProvider.Linux(:powerpc64le, :glibc),
+    BinaryProvider.MacOS(),
+    BinaryProvider.Windows(:i686),
+    BinaryProvider.Windows(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libffi", :libffi)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "libffi", sources, script, platforms, products, dependencies)
+
